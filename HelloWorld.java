7cbd279b2d327deb869442da7d44e026e6ba6b36
Alina Vokorina <vokorinaalina@gmail.com>
2018-04-08 17:11:29 +0300

    add HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+* This is our first Java class
+*/
+public class HelloWorld {
+	public static void main (String [] args) {
+		//Just printing "hello world!"
+		System.out.println ("hello world!");
+	}
+}
