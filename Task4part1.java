0b706277601f4ca42f9b3f2bfe1051bb0c01c3c8
Alina Vokorina <vokorinaalina@gmail.com>
2018-04-28 13:58:27 +0300

    add task4part1

---
+++ Task4part1.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Задание 3.1
+ *
+ * @author Alina Vokorina
+ */
+public class Task4part1 {
+    public static void main(String[] args) {
+        //TODO. Массив размерностью 20, заполняется случайными целыми числами от -10 до 10. Найти максимальный отрицательный и минимальный положительный элементы массива. Поменять их местами.
+        int n = 20;
+        int[] array = new int[n];
+        Random rand = new Random();
+        int i;
+        int maxNegative = -11, minPositive = 11, negativePosition = -1, positivePosition = -1;
+        for (i = 0; i < n; ++i) {
+            int randomInt = rand.nextInt(21) - 10;
+            if ((randomInt < 0) && (randomInt > maxNegative)) {
+                maxNegative = randomInt;
+                negativePosition = i;
+            } else if ((randomInt > 0) && (randomInt < minPositive)) {
+                minPositive = randomInt;
+                positivePosition = i;
+            }
+            array[i] = randomInt;
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(maxNegative + ", " + minPositive);
+        System.out.println(negativePosition + ", " + positivePosition);
+
+        int swap = array[negativePosition];
+        array[negativePosition] = array[positivePosition];
+        array[positivePosition] = swap;
+
+        System.out.println(Arrays.toString(array));
+    }
+}
